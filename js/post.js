fdb02f91c31af4702c30301ca3397e09425ba615
AderbalFilho <aderbalcrato@yahoo.com.br>
2019-09-24 16:29:15 -0300

    refactor(post): Trying to submit payment test

--- js/post.js
+++ js/post.js
@@ -1,22 +1,18 @@
 $(document).ready(function () {
   $("button").click(function () {
     $.ajax({
-      url: 'https://sandbox.pjbank.com.br/recebimentos/e0727263cc7a983f0aae5411ad86c5a144b8ed28/tokens',
+      url: 'https://sandbox.pjbank.com.br/contadigital/e0727263cc7a983f0aae5411ad86c5a144b8ed28/recebimentos/transacoes',
       type: 'post',
       crossDomain: true,
       data: {
-        nome_cartao: $("#nome").val(),
-        numero_cartao: $("#cartao").val(),
-        mes_vencimento: $("#vencimento_mes").val(),
-        ano_vencimento: $("#vencimento_ano").val(),
-        cpf_cartao: $("#cpf").val(),
-        email_cartao: $("#email").val(),
-        celular_cartao: $("#phone").val(),
-        codigo_cvv: $("#cvv").val()
+        descricao_pagamento: $("#description").val(),
+        valor: $("#value").val(),
+        parcelas: $("#installments").val(),
+        token_cartao: $("#token_cartao").val(),
       },
       headers: {
         "Content-Type": "application/json",
-        'X-CHAVE': 'e9db986de751de918ca19a1c377f0b7c313915f8'
+        'X-CHAVE-CONTA': 'e9db986de751de918ca19a1c377f0b7c313915f8'
       },
       success: function (data, status) {
         alert("Data: " + data + "\nStatus: " + status);
